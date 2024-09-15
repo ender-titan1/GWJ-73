f51c53fab273d07cc836cf50c01a905b4aa06db4
LePhroger <132900761+LePhroger@users.noreply.github.com>
2024-09-15 17:55:53 +0200

    made the shop clickable

--- src/shopButton.gd
+++ src/shopButton.gd
@@ -5,6 +5,7 @@ var type_2 = preload("res://icon.svg")
 var type_3 = preload("res://imports/L-Right.png")
 
 var shop_menu: PopupMenu = self.get_popup()
+
 var type = 0
 var rng = RandomNumberGenerator.new()
 
@@ -18,4 +19,15 @@ func _ready():
 			shop_menu.add_icon_item(type_2,"icon")
 		elif type == 3:
 			shop_menu.add_icon_item(type_3,"block")
+			
+	shop_menu.connect("index_pressed", 
+	Callable(self, "_clicked_shop_item"))
 
+func _clicked_shop_item(index: int):
+	match index:
+		0:
+			print(shop_menu.get_item_text(index))
+		1:
+			print(shop_menu.get_item_text(index))
+		2:
+			print(shop_menu.get_item_text(index))
